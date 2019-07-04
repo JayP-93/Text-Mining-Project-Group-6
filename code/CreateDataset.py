28f149bb7744ec18199068442b9174d16e71d3f9
stepan-g <s>
2019-07-04 11:28:14 +0200

    Added comments, code improvement

--- code/CreateDataset.py
+++ code/CreateDataset.py
@@ -1,27 +1,34 @@
-'''
-Create a new dataset with new filenames, and removed metadata. 
-Renamed corpora are in the Datasets/ folder in this repo
+"""
+Create a new dataset with new file names, and removed metadata.
+The results (renamed corpora) are in the Datasets/ folder in this repo
 Original corpora for all languages can be downloaded from MERLIN website
 http://www.merlin-platform.eu/C_data.php
-'''
+"""
 
 import os
 
-dirpath = "/Users/sowmya/Research/CrossLing-Scoring/Corpora/" #CZ_ltext_txt", DE_ltext_txt, IT_ltext_txt
-outputdirpath = "/Users/sowmya/Research/CrossLing-Scoring/Corpora/Renamed/"
+dirpath = "/Users/macbookg/Downloads/meta_ltext"  # path to original corpora folder
+outputdirpath = "/Users/macbookg//Downloads/Renamed/"  # path to folder with output files
 files = os.listdir(dirpath)
 
-inputdirs = ["CZ_ltext_txt", "DE_ltext_txt", "IT_ltext_txt"]
-outputdirs = ["CZ","DE","IT"]
+inputdirs = ["czech", ]  # names of input folders
+outputdirs = ["CZ", "DE", "IT"]  # names of output folders
 
 for i in range(0, len(inputdirs)):
-    files = os.listdir(os.path.join(dirpath,inputdirs[i]))
+    files = os.listdir(os.path.join(dirpath, inputdirs[i]))  # original files
+
+    new_folder = os.path.join(outputdirpath, outputdirs[i])  # folder with output files
+    if not os.path.exists(new_folder):
+        os.makedirs(new_folder)
+
     for file in files:
         print(file)
         if file.endswith(".txt"):
-            content = open(os.path.join(dirpath,inputdirs[i],file),"r").read()
+            content = open(os.path.join(dirpath, inputdirs[i], file), "r").read()
             cefr = content.split("Learner text:")[0].split("Overall CEFR rating: ")[1].split("\n")[0]
-            newname = file.replace(".txt","") + "_" + outputdirs[i] + "_" + cefr + ".txt"
-            fh = open(os.path.join(outputdirpath,outputdirs[i],newname), "w")
-            text = content.split("Learner text:")[1].strip()
-            print("wrote: ", os.path.join(outputdirpath,outputdirs[i],newname))
+            newname = file.replace(".txt", "") + "_" + outputdirs[i] + "_" + cefr + ".txt"  # name of new file
+            fh = open(os.path.join(new_folder, newname), "w")  # new file
+            text = content.split("Learner text:")[1].strip()  # raw text
+            fh.write(text)  # write to new file
+            fh.close()
+            print("wrote: ", os.path.join(outputdirpath, outputdirs[i], newname))
--- code/ErrorStats.py
+++ code/ErrorStats.py
@@ -1,19 +1,33 @@
 """
-Purpose: Knowing error statistics in the data for DE and IT using LanguageTool
+Saves information about errors (using Language check) in DE and IT data to text files.
+The main purpose: knowledge about error statistics in the data for DE and IT.
 """
 
 import language_check
-import os, collections, pprint
+import os
 
-def write_featurelist(file_path,some_list):
+
+def write_featurelist(file_path, some_list):
+    """
+    Creates file with data from list (each element of list is a new line in text file)
+    :param file_path: path to the file
+    :param some_list: list with data
+    """
     fh = open(file_path, "w")
     for item in some_list:
-      fh.write(item)
-      fh.write("\n")
+        fh.write(item)
+        fh.write("\n")
     fh.close()
 
-def error_stats(inputpath,lang,output_path):
-    files = os.listdir(inputpath)
+
+def error_stats(inputpath, lang, output_path):
+    """
+    Creates three text files with information of different errors in input texts.
+    :param inputpath: path to folder with input data
+    :param lang: string with name of language, e.g. 'de'
+    :param output_path: path to the output text files
+    """
+    files = os.listdir(inputpath)  # input files
     checker = language_check.LanguageTool(lang)
     rules = {}
     locqualityissuetypes = {}
@@ -21,24 +35,28 @@ def error_stats(inputpath,lang,output_path):
 
     for file in files:
         if file.endswith(".txt"):
-            text = open(os.path.join(inputpath,file)).read()
+            text = open(os.path.join(inputpath, file)).read()
             matches = checker.check(text)
             for match in matches:
                 rule = match.ruleId
                 loc = match.locqualityissuetype
                 cat = match.category
-                rules[rule] = rules.get(rule,0) +1
-                locqualityissuetypes[loc] = locqualityissuetypes.get(loc,0) +1
-                categories[cat] = categories.get(cat,0)+1
+                rules[rule] = rules.get(rule, 0) + 1
+                locqualityissuetypes[loc] = locqualityissuetypes.get(loc, 0) + 1
+                categories[cat] = categories.get(cat, 0) + 1
 
-    write_featurelist(output_path+lang+"-rules.txt", sorted(rules.keys()))
-    write_featurelist(output_path+lang+"-locquality.txt", sorted(locqualityissuetypes.keys()))
-    write_featurelist(output_path+lang+"-errorcats.txt", sorted(categories.keys()))
-
-inputpath_de = "/home/bangaru/GitProjects/CrossLingualScoring/Datasets/DE/"
-inputpath_it = "/home/bangaru/GitProjects/CrossLingualScoring/Datasets/IT/"
-
-error_stats(inputpath_de, "de", "../features/")
-error_stats(inputpath_it, "it","../features/")
+    write_featurelist(output_path + lang + "-rules.txt", sorted(rules.keys()))
+    write_featurelist(output_path + lang + "-locquality.txt", sorted(locqualityissuetypes.keys()))
+    write_featurelist(output_path + lang + "-errorcats.txt", sorted(categories.keys()))
 
 
+def main():
+    inputpath_de = "../Datasets/DE/"
+    inputpath_it = "../Datasets/IT/"
+
+    error_stats(inputpath_de, "de", "../features/")
+    error_stats(inputpath_it, "it", "../features/")
+
+
+if __name__ == "__main__":
+    main()
