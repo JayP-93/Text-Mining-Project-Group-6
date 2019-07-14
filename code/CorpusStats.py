ac51c4d31867e99a4e84e878786375c664e8a592
Cristinutaa <cristina.arhiliuc@insa-lyon.fr>
2019-07-14 10:20:19 +0200

    Modifications on the statistics file CorpusStats.py
    Deleted from data the files with XX label

--- code/CorpusStats.py
+++ code/CorpusStats.py
@@ -13,41 +13,24 @@ files = os.listdir(corpora_path)
 for i in range(0, len(input_dir_names)):
     files = os.listdir(os.path.join(corpora_path, input_dir_names[i]))  # original files
     number_of_docs = 0
-    number_of_words = 0
-    num_a1 = 0
-    num_a2 = 0
-    num_b1 = 0
-    num_b2 = 0
-    num_c1 = 0
-    num_c2 = 0
+    number_of_words = [0]*6
+    num_doc_label = [0]*6
+    label_list = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2']
     for file in files:
         if file.endswith(".txt"):
             number_of_docs += 1
-            content = open(os.path.join(corpora_path, input_dir_names[i], file), "r").read()
-
-            number_of_words += sum([word.strip(string.punctuation).isalpha() for word in content.split()])
+            content = open(os.path.join(corpora_path, input_dir_names[i], file), "r", encoding="utf-8").read()
             cefr = file.split(".txt")[0].split("_")[-1]  # language level
-
-            if cefr == 'A1':
-                num_a1 += 1
-            if cefr == 'A2':
-                num_a2 += 1
-            if cefr == 'B1':
-                num_b1 += 1
-            if cefr == 'B2':
-                num_b2 += 1
-            if cefr == 'C1':
-                num_c1 += 1
-            if cefr == 'C2':
-                num_c2 += 1
+            index = label_list.index(cefr)
+            number_of_words[index] += sum([word.strip(string.punctuation).isalpha() for word in content.split()])
+            num_doc_label[index] += 1
 
     print('Language:', input_dir_names[i])
     print('Number of documents:', number_of_docs)
-    print('Average number of words:', number_of_words / number_of_docs)
-    print('A1: \t', num_a1)
-    print('A2: \t', num_a2)
-    print('B1: \t', num_b1)
-    print('B2: \t', num_b2)
-    print('C1: \t', num_c1)
-    print('C2: \t', num_c2)
+    for j, label in enumerate(label_list):
+        average_words = 0
+        if not num_doc_label[j]:
+            print('%s : \t %d , average words: %3.2f' % (label, 0, 0))
+        else:
+            print('%s : \t %d , average words: %3.2f' % (label, num_doc_label[j], number_of_words[j]/num_doc_label[j]))
     print()
